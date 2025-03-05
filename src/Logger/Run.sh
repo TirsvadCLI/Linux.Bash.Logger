1aa1e6c26456d80ddc2f8a8fa7c158cb50954461
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-03-05 15:26:57 +0800

    constants to a seperated tile

--- src/Logger/Run.sh
+++ src/Logger/Run.sh
@@ -8,32 +8,13 @@
 ## @author Jens Tirsvad Nielsen
 ## @date May 2023
 
+## @brief string internal field separator
+IFS=$'\n\t'
+
 ## @brief string script directory
 declare -g TCLI_LINUX_BASH_LOGGER_SCRIPTDIR="$(dirname "$(realpath "${BASH_SOURCE}")")"
-## @brief string version
-declare -g TCLI_LINUX_BASH_LOGGER="0.3.2"
-## @brief string internal field separator
-declare -g IFS=$'\n\t'
-## @brief bool if warning have been triggered
-declare -g TCLI_LINUX_BASH_LOGGER_INFOSCREEN_WARN=0
-## @brief string color no color
-declare -g TCLI_LINUX_BASH_LOGGER_NC='\033[0m' # No Color
-## @brief string color red
-declare -g TCLI_LINUX_BASH_LOGGER_RED='\033[0;31m'
-## @brief string color green
-declare -g TCLI_LINUX_BASH_LOGGER_GREEN='\033[0;32m'
-## @brief string color brown
-declare -g TCLI_LINUX_BASH_LOGGER_BROWN='\033[0;33m'
-## @brief string color blue
-declare -g TCLI_LINUX_BASH_LOGGER_BLUE='\033[0;34m'
-## @brief string color yellow
-declare -g TCLI_LINUX_BASH_LOGGER_YELLOW='\033[1;33m'
-## @brief string color white
-declare -g TCLI_LINUX_BASH_LOGGER_WHITE='\033[0;37m'
-## @brief back to line
-declare -g TCLI_LINUX_BASH_BACK_TO_LINE='\r\033[1C'
-## @brief log to file
-declare -g -i TCLI_LINUX_BASH_LOGGER_TO_FILE=1
+
+. $TCLI_LINUX_BASH_LOGGER_SCRIPTDIR/inc/constants.sh
 
 ## @fn tcli_linux_bash_logger_init()
 ## @details
